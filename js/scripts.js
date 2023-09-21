8fc71c60262b4811554e01148ec6b566fe54b444
thoriqre <117810381+thoriqre@users.noreply.github.com>
2023-09-22 00:35:29 +0700

    Day 3 (DAFFA)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownLinks = document.querySelectorAll(".footer-nav ul li.dropdown > a");
+    const colorBackground = document.getElementById("color-background");
+  
+    dropdownLinks.forEach(function (link) {
+      link.addEventListener("mouseover", function () {
+        const computedStyle = window.getComputedStyle(link);
+        const linkColor = computedStyle.getPropertyValue("color");
+        colorBackground.style.backgroundColor = linkColor;
+      });
+  
+      link.addEventListener("mouseout", function () {
+        colorBackground.style.backgroundColor = "transparent";
+      });
+    });
+  });
+  
